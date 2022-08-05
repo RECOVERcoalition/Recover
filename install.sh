4060976d60f9a7277e9741b24f1a3e465d9d821a
farzaneh labbaf <f.labbaf97@gmail.com>
2022-08-05 15:05:08 +0200

    Make changes to also install on mac with apple processor

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-conda create --name recover python=3.6
+conda create --name recover python=3.9
 conda activate recover
 
 # Install pip
@@ -7,7 +7,13 @@ conda install -c anaconda pip
 # Install requirements
 pip install -r requirements.txt
 
+# Install pytorch to support GPU on mac m1
+# Expect the M1-GPU support to be included in the next stable release. For the time being, it only is found in the Nightly release.
+conda install pytorch -c pytorch-nightly
+
 # Install ray
+# Firts make sure grpcio is only installed with conda
+pip uninstall grpcio; conda install grpcio=1.43.0
 pip install -U "ray[tune]"
 
 # Install recover
