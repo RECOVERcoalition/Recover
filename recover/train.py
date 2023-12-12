0ed04bd10863a67e7f6b40178bb9ba06ef18f8a8
Pavithya M B D <dhanumapitigama@gmail.com>
2023-12-12 19:46:24 +0530

    Bayesian methods implemented

--- recover/train.py
+++ recover/train.py
@@ -1,4 +1,5 @@
 import torch
+import torchbnn as bnn
 import os
 from torch.utils.data import DataLoader
 from recover.utils.utils import get_tensor_dataset, trial_dirname_creator
@@ -8,6 +9,7 @@ import ray
 import time
 import argparse
 import numpy as np
+import pandas as pd
 import importlib
 from scipy import stats
 from scipy.stats import spearmanr
@@ -55,6 +57,55 @@ def train_epoch(data, loader, model, optim):
 
     return summary_dict
 
+"""
+The cost function is updated to consider both MSE loss and KL loss
+KL weight is set to 0.01
+Default model loss is unchanged - MSE (For comparision with Rec
+"""
+def bayesian_train_epoch(data, loader, model, optim):
+
+    model.train()
+
+    epoch_loss = 0
+    num_batches = len(loader)
+
+    all_mean_preds = []
+    all_targets = []
+    
+    kl_loss = bnn.BKLLoss(reduction='mean', last_layer_only=False)
+    kl_weight = 0.01
+
+    for _, drug_drug_batch in enumerate(loader):
+        optim.zero_grad()
+
+        out = model.forward(data, drug_drug_batch)
+
+        # Save all predictions and targets
+        all_mean_preds.extend(out.mean(dim=1).tolist())
+        all_targets.extend(drug_drug_batch[2].tolist())
+
+        loss = model.loss(out, drug_drug_batch) #MSE
+        kl = kl_loss(model)
+        
+        cost = loss + kl_weight*kl
+
+        cost.backward()
+        optim.step()
+
+        epoch_loss += loss.item()
+
+    epoch_comb_r_squared = stats.linregress(all_mean_preds, all_targets).rvalue**2
+
+    summary_dict = {
+        "loss_mean": epoch_loss / num_batches,
+        "comb_r_squared": epoch_comb_r_squared
+        #"cost": cost.item()
+    }
+
+    print("Training", summary_dict)
+
+    return summary_dict
+
 
 def eval_epoch(data, loader, model):
     model.eval()
@@ -92,6 +143,56 @@ def eval_epoch(data, loader, model):
     all_out = torch.cat(all_out)
 
     return summary_dict, all_out
+
+
+def bayesian_eval_epoch(data, loader, model):
+    model.eval()
+
+    epoch_loss = 0
+    num_batches = len(loader)
+
+    all_out = []
+    all_mean_preds = []
+    all_targets = []
+    all_combs = []
+
+    with torch.no_grad():
+        for _, drug_drug_batch in enumerate(loader):
+            out = model.forward(data, drug_drug_batch)
+
+            # Save all predictions and targets - rec_id_to_idx_dict
+            all_out.append(out)
+            all_mean_preds.extend(out.mean(dim=1).tolist())
+            all_targets.extend(drug_drug_batch[2].tolist())
+            all_combs.extend(drug_drug_batch[0].tolist())
+
+            loss = model.loss(out, drug_drug_batch)
+            epoch_loss += loss.item()
+        epoch_comb_r_squared = stats.linregress(all_mean_preds, all_targets).rvalue**2
+        epoch_spear = spearmanr(all_targets, all_mean_preds).correlation
+
+    summary_dict = {
+        "loss_mean": epoch_loss / num_batches,
+        "comb_r_squared": epoch_comb_r_squared,
+        "spearman": epoch_spear
+    }
+    
+    print("Testing", summary_dict, '\n')
+
+    all_out = torch.cat(all_out)
+
+    return summary_dict, all_out, all_combs
+
+"""
+Custom Aggregration function for obtaining results when training with multi cell-lines 
+
+Functionality: group data by the combination (permute invarient) and get maximum mean synergy score and the corresponding std
+"""
+def custom_agg(group):
+    max_row = group.loc[group['mean'].idxmax()]
+    return pd.Series({'combination': max_row['combination'],
+                  'mean': max_row['mean'],
+                  'std': max_row['std']})
 
 
 ########################################################################################################################
@@ -213,10 +314,189 @@ class BasicTrainer(tune.Trainable):
     def save_checkpoint(self, checkpoint_dir):
         checkpoint_path = os.path.join(checkpoint_dir, "model.pth")
         torch.save(self.model.state_dict(), checkpoint_path)
-        return None
+        return checkpoint_dir
 
     def load_checkpoint(self, checkpoint_path):
         self.model.load_state_dict(torch.load(checkpoint_path))
+
+
+########################################################################################################################
+# Bayesian Basic trainer
+########################################################################################################################
+
+
+class BayesianBasicTrainer(tune.Trainable):
+    def setup(self, config):
+        print("Initializing regular training pipeline")
+
+        self.batch_size = config["batch_size"]
+        device_type = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = torch.device(device_type)
+        self.training_it = 0
+
+        # Initialize dataset
+        dataset = config["dataset"](
+            fp_bits=config["fp_bits"],
+            fp_radius=config["fp_radius"],
+            cell_line=config["cell_line"],
+            study_name=config["study_name"],
+            in_house_data=config["in_house_data"],
+            rounds_to_include=config["rounds_to_include"],
+        )
+        
+        self.patience_stop = config["stop"]["patience"]
+        self.max_iter = config["stop"]["training_iteration"]
+
+        self.data = dataset.data.to(self.device)
+
+        # If a score is the target, we store it in the ddi_edge_response attribute of the data object
+        if "target" in config.keys():
+            possible_target_dicts = {
+                "bliss_max": self.data.ddi_edge_bliss_max,
+                "bliss_av": self.data.ddi_edge_bliss_av,
+                "css_av": self.data.ddi_edge_css_av,
+            }
+            self.data.ddi_edge_response = possible_target_dicts[config["target"]]
+
+        torch.manual_seed(config["seed"])
+        np.random.seed(config["seed"])
+
+        # Perform train/valid/test split. Test split is fixed regardless of the user defined seed
+        self.train_idxs, self.val_idxs, self.test_idxs = dataset.random_split(config)
+
+        # Train loader
+        train_ddi_dataset = get_tensor_dataset(self.data, self.train_idxs)
+
+        self.train_loader = DataLoader(
+            train_ddi_dataset,
+            batch_size=config["batch_size"]
+        )
+
+        # Valid loader
+        valid_ddi_dataset = get_tensor_dataset(self.data, self.val_idxs)
+
+        self.valid_loader = DataLoader(
+            valid_ddi_dataset,
+            batch_size=config["batch_size"]
+        )
+        
+        # Test loader
+        test_ddi_dataset = get_tensor_dataset(self.data, self.test_idxs)
+
+        self.test_loader = DataLoader(
+            test_ddi_dataset,
+            batch_size=config["batch_size"]
+        )
+
+
+        # Initialize model
+        self.model = config["model"](self.data, config)
+
+        # Initialize model with weights from file
+        load_model_weights = config.get("load_model_weights", False)
+        if load_model_weights:
+            model_weights_file = config.get("model_weights_file")
+            model_weights = torch.load(model_weights_file, map_location="cpu")
+            self.model.load_state_dict(model_weights)
+            print("pretrained weights loaded")
+        else:
+            print("model initialized randomly")
+
+        self.model = self.model.to(self.device)
+        print(self.model)
+
+        # Initialize optimizer
+        self.optim = torch.optim.Adam(
+            self.model.parameters(),
+            lr=config["lr"],
+            weight_decay=config["weight_decay"],
+        )
+
+        self.train_epoch = config["train_epoch"]
+        self.eval_epoch = config["eval_epoch"]
+
+        self.patience = 0
+        self.max_eval_r_squared = -1
+
+    def step(self):
+    
+        num_realizations = 10
+
+        train_metrics = self.train_epoch(
+            self.data,
+            self.train_loader,
+            self.model,
+            self.optim,
+        )
+
+        eval_metrics, _, _ = self.eval_epoch(self.data, self.valid_loader, self.model)
+
+        train_metrics = [("train/" + k, v) for k, v in train_metrics.items()]
+        eval_metrics = [("eval/" + k, v) for k, v in eval_metrics.items()]
+
+        metrics = dict(train_metrics + eval_metrics)
+        
+        metrics["training_iteration"] = self.training_it
+        self.training_it += 1
+                
+        # Compute patience
+        if metrics['eval/comb_r_squared'] > self.max_eval_r_squared:
+            self.patience = 0
+            self.max_eval_r_squared = metrics['eval/comb_r_squared']
+        else:
+            self.patience += 1
+            
+        metrics['patience'] = self.patience
+        metrics['all_space_explored'] = 0                        
+
+
+        if ((self.patience >= self.patience_stop) | (self.training_it > self.max_iter)):
+            test_result = {}
+            realization_results, result_synergy, drug_combs = self.eval_epoch(self.data, self.test_loader, self.model)
+            drug_combinations_synergy = {'data': self.test_idxs}
+            test_metrics = dict([("test/" + k, [v]) for k, v in realization_results.items()])
+            for i in range(num_realizations-1):
+                realization_results, new_synergy, _ = self.eval_epoch(self.data, self.test_loader, self.model)
+                result_synergy = torch.cat((result_synergy, new_synergy), dim=1)
+                for k, v in realization_results.items():
+                    test_metrics["test/" + k].append(v)
+                    
+            for key in test_metrics:
+                test_result[str(key)+ "/mean"] = np.mean(test_metrics[key])
+                test_result[str(key)+ "/std"] = np.std(test_metrics[key])
+                
+            
+            synergy_mean = list(torch.mean(result_synergy, dim=1).numpy())
+            synergy_std = list(torch.std(result_synergy, dim=1).numpy())
+            
+            result_tuples = list(map(lambda inner_list: tuple(sorted(inner_list)), drug_combs))
+            
+            dataset = pd.DataFrame({'combination': result_tuples, 'mean': synergy_mean, 'std': synergy_std }, columns=['combination', 'mean', 'std'])
+            
+            result_df = dataset.groupby('combination').apply(custom_agg) # Do custom aggregration  - Not needed when working on one cell-line but no difference in results
+            
+            result_df.reset_index(drop=True, inplace=True)
+            
+            metrics.update(dict(test_result)) # Add test results to output files
+            
+            metrics['synergy_combs'] = list(result_df['combination'])
+            metrics['synergy_mean'] = list(result_df['mean'])
+            metrics['synergy_std'] = list(result_df['std'])
+            
+            #metrics.update(dict(drug_combinations_synergy))
+        
+        return metrics
+    
+    
+    def save_checkpoint(self, checkpoint_dir):
+        checkpoint_path = os.path.join(checkpoint_dir, "model.pth")
+        torch.save(self.model.state_dict(), checkpoint_path)
+        
+        return checkpoint_dir
+
+    def load_checkpoint(self, checkpoint_path):
+        self.model.load_state_dict(torch.load(checkpoint_path))
+
 
 
 ########################################################################################################################
